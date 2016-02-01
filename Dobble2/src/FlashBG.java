7ef24859d8c51e00391bfbaf676ef571dd23493e
Benjamin <Benjamin@Ben-pc>
2016-02-01 15:52:47 +0100

    commencement flashBG

---
+++ Dobble2/src/FlashBG.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+public class FlashBG implements Runnable{
+
+	public static final Color NEUTRE = new Color(150, 150, 255);
+	public static final Color GAGNE = new Color(150, 255, 150);
+	public static final Color PERDU = new Color(255, 150, 150);
+
+	public void flash(Color c){
+		
+	}
+	
+	@Override
+	public void run() 
+	{
+		
+	}
+
+}
